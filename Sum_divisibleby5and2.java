85e0ebdfa3828ed6edcd3e3189d71db5b684f8b6
komaragiriMuraliPriya <121532826+komaragiriMuraliPriya@users.noreply.github.com>
2024-03-13 18:19:11 +0530

    Update Sum_divisibleby5and2.java

--- Sum_divisibleby5and2.java
+++ Sum_divisibleby5and2.java
@@ -1,12 +1,15 @@
-public class Main {
+class A {
     public static void main(String[] args) {
-        int[] values = {100, 52, 43, 99, 42, 40, 91};    
-        int sum = 0;       
-        for (int value : values) {
-            if (value % 5 == 0 && value % 2 == 0) {
-                sum += value;
+        int arr[]={100,52,43,99,42,40,91};
+        int i,s=0;
+        
+        for (i=0;i<(arr.length);i++)
+        {
+            if (arr[i]%5==0 || arr[i]%2==0)
+            {
+                s=s+arr[i];
             }
         }
-        System.out.println("Sum of numbers divisible by 5 and 2: " + sum);
+        System.out.print(s);
     }
 }
